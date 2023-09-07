ce50688b42feca15f2a8d3dae167b649f9b1d540
Xiaolong Liu <144281186+Alwaysvvvv@users.noreply.github.com>
2023-09-07 10:10:18 -0500

    do some test mentioned in lecture1

---
+++ my_first_python.py
@@ -0,0 +1,17 @@
+
+from utilities import cube_list
+import names
+print("Python Program is running")
+
+
+#cube_list()
+
+def main():
+    one_name = names.get_full_name()
+    print("one name: ")
+    print(one_name)
+
+if __name__ == "__main__":
+    main()
+
+
---
+++ utilities.py
@@ -0,0 +1,12 @@
+def cube_list(upper_bound):
+    for i in range(upper_bound):
+        print(i**3)
+
+
+def test_cube_list(upper_bound):
+    for i in range(upper_bound):
+        print(i**3)
+
+if __name__ == "__main__":
+    test_cube_list(2)
+
